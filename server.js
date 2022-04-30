6de539d47b047b7658d11a998edf103294bec3c5
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-04-30 14:08:16 -0400

    Cleans up server file

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// Require dependencies 
-const express = require("express");
+// Require dependencies
+const express = require("express")
 const path = require('path')
 
-
-// Assign dependencies 
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
+// Assign dependencies
+const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
 
 // Listener compatible with Heroku, Localhost
-let port = process.env.PORT;
-if (port === null || port === "" || port === undefined ) { port = 3000; };
-app.listen(port, () => console.log(`Server accessible at port ${port}.`));
-// END Listener 
+let port = process.env.PORT
+if (port === null || port === "" || port === undefined ) { port = 3000; }
+app.listen(port, () => console.log(`Server accessible at port ${port}.`))
+// END Listener
 
 // END of document
