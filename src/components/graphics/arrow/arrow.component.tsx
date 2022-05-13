ed986e8a264f89eb358bc6bd514f42b4c4e02bdb
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-05-13 16:23:32 -0400

    WIP Finding location on connector click
    Having trouble getting correct board position from event handler.

--- src/components/graphics/arrow/arrow.component.tsx
+++ src/components/graphics/arrow/arrow.component.tsx
@@ -16,10 +16,29 @@ const Arrow = (props: any) => {
     dispatch({ type: 'SET_ALL_ARROWS', payload: {arrowArray: newArrowArray}})
   }
 
+  function zoneFinder(e: any) {
+    let data = props.arrowData
+    let x1 = data.originPos.x
+    let y1 = data.originPos.y
+    let x2 = data.endPos.x
+    let y2 = data.endPos.y
+    let clickX = e.window
+    let clickY = e.target
+
+    console.log(clickY)
+    console.log(e)
+    console.log(x1)
+    console.log(e.target.x1.baseVal.value, e.target.y1.baseVal.value)
+    console.log(e.target.x2.baseVal.value, e.target.y2.baseVal.value)
+    // console.log(e.target)
+
+  }
+
   return (
     <line
       style={{ pointerEvents: 'all', zIndex: '-2147483647', position: 'relative', cursor: 'crosshair' }}
       onDoubleClick={() => arrowDelete(props.id) }
+      onMouseDown={zoneFinder}
       x1={props.arrowData.originPos.x}
       y1={props.arrowData.originPos.y}
       x2={props.arrowData.endPos.x}
